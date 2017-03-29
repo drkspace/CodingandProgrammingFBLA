11664a687f8bc5f065a227bc5dc640c25e75b6d8
Daniel Kramer <drkspace@gmail.com>
2017-03-29 09:59:04 -0400

    update

--- .compile.py
+++
@@ -1,6 +0,0 @@
-from subprocess import call
-call(["python", "releases/pyinstaller.py", "--onefile", "fec.py"])
-call(["mv", "dist/fec", "releases/"])
-call(["rm", "-r", "dist"])
-call(["rm", "-r", "build"])
-call(["rm", "fec.spec"])
